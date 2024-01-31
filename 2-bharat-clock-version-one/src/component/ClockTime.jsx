ccc00a721db19ab08a5587c8522bd4be41aaa470
mgmanoj2018 <mgmanoj2018@gmail.com>
2024-01-31 18:30:09 +0530

    useEffect and clear useEffect

--- 2-bharat-clock-version-one/src/component/ClockTime.jsx
+++ 2-bharat-clock-version-one/src/component/ClockTime.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const ClockTime = () => {
-    const time = new Date()
+  const [time, setTime] =  useState(new Date())
+  useEffect(()=>{
+    console.log("Interval is setUp")
+      const timeInterval = setInterval(()=>{
+        setTime(new Date())
+      },1000)
+    return ()=>{
+      clearInterval(timeInterval)
+      console.log("set time out" + " " + timeInterval)
+    }
+  },[])
+    
   return (
     <div>
         <p>This is current time :{time.toDateString()} - {time.toLocaleTimeString()}
